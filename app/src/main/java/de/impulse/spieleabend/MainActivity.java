4dc1af02ea94ea8edb094c2fa3574285667681ce
Erik Daas <erik.daas.apps@outlook.de>
2026-04-21 13:55:59 +0200

    Eingeschmolzen aufs Nötigste

--- app/src/main/java/de/impulse/spieleabend/MainActivity.java
+++ app/src/main/java/de/impulse/spieleabend/MainActivity.java
@@ -1,11 +1,11 @@
 package de.impulse.spieleabend;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
